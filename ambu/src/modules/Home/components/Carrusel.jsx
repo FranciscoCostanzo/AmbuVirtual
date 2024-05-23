6139d315c7e579b83f07b32a96c2f59f49c84a62
CarpinchoRecu <123523494+CarpinchoRecu@users.noreply.github.com>
2024-05-23 15:21:28 -0300

    terminando Home

--- ambu/src/modules/Home/components/Carrusel.jsx
+++ ambu/src/modules/Home/components/Carrusel.jsx
@@ -89,11 +89,8 @@ const Carrusel = ({ slides }) => {
         <div className="flecha" onClick={handlePreviousSlide}>
         <svg
             viewBox="0 0 24 24"
-            stroke-width="1.5"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M15 6l-6 6l6 6" />
@@ -102,11 +99,8 @@ const Carrusel = ({ slides }) => {
         <div className="flecha" onClick={handleNextSlide}>
         <svg
             viewBox="0 0 24 24"
-            stroke-width="1.5"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M9 6l6 6l-6 6" />
